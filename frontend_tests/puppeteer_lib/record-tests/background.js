e4abb38f3eaf09af95fab08011e1349cc3aa082d
Priyank Patel <priyankp390@gmail.com>
2020-07-30 18:03:06 +0000

    debug: Original code of puppetcam.

---
+++ frontend_tests/puppeteer_lib/record-tests/background.js
@@ -0,0 +1,92 @@
+/* global chrome, MediaRecorder, FileReader */
+
+let recorder = null;
+let filename = null;
+
+function startRecording(port, oldTitle) {
+    chrome.desktopCapture.chooseDesktopMedia(["tab", "audio"], async (streamId) => {
+        const options = {
+            audio: {
+                mandatory: {
+                    chromeMediaSource: "system",
+                },
+            },
+            video: {
+                mandatory: {
+                    chromeMediaSource: "desktop",
+                    chromeMediaSourceId: streamId,
+                    minWidth: 1280,
+                    maxWidth: 1280,
+                    minHeight: 720,
+                    maxHeight: 720,
+                    minFrameRate: 60,
+                },
+            },
+        };
+
+        const stream = await navigator.mediaDevices.getUserMedia(options);
+        port.postMessage({oldTitle});
+        recorder = new MediaRecorder(stream, {
+            videoBitsPerSecond: 2500000,
+            ignoreMutedMedia: true,
+            mimeType: "video/webm",
+        });
+
+        const vedioChunks = [];
+        recorder.ondataavailable = function (event) {
+            if (event.data.size > 0) {
+                vedioChunks.push(event.data);
+            }
+        };
+
+        recorder.onstop = function (event) {
+            const buffer = new Blob(vedioChunks, {
+                type: "video/webm",
+            });
+
+            const url = URL.createObjectURL(buffer);
+            chrome.downloads.download({
+                url,
+                filename,
+            });
+        };
+
+        recorder.start();
+    });
+}
+
+chrome.runtime.onConnect.addListener((port) => {
+    port.onMessage.addListener(async (msg) => {
+        console.log(msg);
+        switch (msg.type) {
+            case "SET_EXPORT_PATH":
+                filename = msg.filename;
+                break;
+
+            case "REC_STOP":
+                recorder.stop();
+                break;
+
+            case "REC_CLIENT_PLAY":
+                if (recorder) {
+                    return;
+                }
+
+                startRecording(port, msg.data.oldTitle);
+                break;
+        }
+    });
+
+    // Wait until download completes...
+    chrome.downloads.onChanged.addListener((delta) => {
+        if (!delta.state || delta.state.current !== "complete") {
+            return;
+        }
+
+        try {
+            port.postMessage({downloadComplete: true});
+        } catch (e) {
+            console.error(e);
+        }
+    });
+});
---
+++ frontend_tests/puppeteer_lib/record-tests/content_script.js
@@ -0,0 +1,36 @@
+const recorderInjectedProp = "__$$recorder_injected__";
+if (!window[recorderInjectedProp]) {
+    Object.defineProperty(window, recorderInjectedProp, {value: true, writable: false});
+
+    const port = chrome.runtime.connect(chrome.runtime.id);
+    port.onMessage.addListener((msg) => window.postMessage(msg, "*"));
+    window.addEventListener("message", (event) => {
+        // Relay client messages
+        if (event.source === window && event.data.type) {
+            port.postMessage(event.data);
+        }
+
+        if (event.data.type === "PLAYBACK_COMPLETE") {
+            port.postMessage({type: "REC_STOP"}, "*");
+        }
+
+        if (event.data.downloadComplete) {
+            document.querySelector("html").classList.add("__download_complete__");
+        }
+
+        if (event.data.oldTitle) {
+            if (document.title === "PuppeteerRecording") {
+                document.title = event.data.oldTitle;
+            }
+        }
+    });
+
+    window.addEventListener("load", () => {
+        // The document.title set there must be in sync with
+        // --auto-select-desktop-capture-source=PuppeteerRecording
+        // we pass to puppeteer.lauch arg options.
+        const oldTitle = document.title;
+        document.title = "PuppeteerRecording";
+        window.postMessage({type: "REC_CLIENT_PLAY", data: {oldTitle}}, "*");
+    });
+}
