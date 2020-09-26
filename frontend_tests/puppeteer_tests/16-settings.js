3199db0705f90d7cc7a83c02da58b3f7922ab2a3
Dinesh <chdinesh1089@gmail.com>
2020-09-26 17:27:27 +0000

    log passwords too

--- frontend_tests/puppeteer_tests/16-settings.js
+++ frontend_tests/puppeteer_tests/16-settings.js
@@ -53,7 +53,9 @@ async function test_change_password(page) {
     await page.waitForSelector(change_password_button_selector, {visible: true});
 
     await page.type("#old_password", test_credentials.default_user.password);
+    await page.evaluate(() => console.log('old pass', $("#old_password").val()));
     await page.type("#new_password", "new_password");
+    await page.evaluate(() => console.log('new pass', $("#new_password").val()));
     await page.click(change_password_button_selector);
 
     // On success the change password modal gets closed.
@@ -69,6 +71,7 @@ async function test_get_api_key(page) {
     await common.fill_form(page, "#api_key_form", {
         password: test_credentials.default_user.password,
     });
+    await page.evaluate(() => console.log('api pass', $("#new_password").val()));
     await page.click(get_api_key_button_selector);
 
     await page.waitForSelector("#show_api_key", {visible: true});
